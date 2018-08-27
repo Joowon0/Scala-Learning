39d61be7c0ff8644932fb29a18e519298728fd37
Joowon <joowon7508@gmail.com>
2018-08-27 22:45:48 +0900

    I am stupid

--- 1_Functional_Programming_Principles_in_Scala/week2/HigherOrderFunctions.scala
+++ 1_Functional_Programming_Principles_in_Scala/week2/HigherOrderFunctions.scala
@@ -10,10 +10,10 @@ object HigherOrderFunctions {
   def sum : (Int => Int) => (Int, Int) => Int = GofF((x, y) => x+y, 0)
   def product : (Int => Int) => (Int, Int) => Int = GofF((x, y) => x*y, 1)
 
-
-  def sumofInts(a: Int, b: Int) = sum (x => x) ( a, b )
-  def sumOfCubes(a: Int, b: Int) = sum(x => x * x * x) ( a, b )
-  def sumOfFcatorial(a: Int, b: Int) = sum(x => factorial(x)) ( a, b)
+  
+  def sumofInts      : (Int, Int) => Int = sum (x => x)
+  def sumOfCubes     : (Int, Int) => Int = sum (x => x * x * x)
+  def sumOfFcatorial : (Int, Int) => Int = sum (x => factorial(x))
 
   def factorial(n: Int) = product (x => x) (1, n)
 }
